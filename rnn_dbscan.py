f90e6609aaf8e00eb9e46e5713cf86da33326058
luk-f <lucasfoulon@gmail.com>
2021-08-20 17:31:25 +0200

    first version
    TODO to optimize

--- rnn_dbscan.py
+++ rnn_dbscan.py
@@ -3,99 +3,141 @@ from sklearn.neighbors import NearestNeighbors
 from scipy.spatial.distance import pdist, euclidean
 
 # enumerate rnn
-def enum_rnn(X_indices, x_indice):
-    return np.where(np.any(X_indices == x_indice, axis=1))
+def enum_rnn(X_indices: np.ndarray, x_indice: int) -> np.array:
+    rnn_indices = np.where(np.any(np.array(X_indices) == x_indice, axis=1))[0]
+    return rnn_indices[rnn_indices != x_indice]
+
+def rnn_size(rnn_indices):
+    return np.bincount(rnn_indices.flatten(), minlength=rnn_indices.shape[0])
 
 def density_cluster(cluster):
     return max(pdist(cluster))
 
 class RNN_DBSCAN:
 
-    def __init__(self, k, X):
-        self.k = k
+    def __init__(self, X: np.ndarray, k: int = 10):
+        """
+        Init RNN-DBSCAN
+
+        :param X: les objets à clusteriser
+        :type X: np.ndarray
+        :param k: le paramètre de voisinage
+        :type k: int
+        """
         self.X = X
+        self.k = k
 
         # voisinage 
         nbrs = NearestNeighbors(n_neighbors=self.k, algorithm='ball_tree').fit(self.X)
         self.indices = nbrs.kneighbors(self.X, return_distance=False)
-        self.size_Rk = np.zeros(len(self.X), dtype=np.dtype('int32'))
-        for i in range(len(self.X)):
-            enum_rnn(self.indices, i)
+        self.size_Rk = rnn_size(self.indices)
 
         self.assign = np.zeros(len(self.X), dtype=np.dtype('int32'))
 
-    def rnn_dbscan(self):
+    def rnn_dbscan(self) -> np.array:
         """
-        algo principal pour assigner une classe aux objets du dataset `X`
+        Algorithme principal pour assigner une classe aux objets du dataset `X`
         `k` le paramètre de l'algo RNN-DBSCAN
+
+        :return: le groupe d'appartenance de chaque objet
+        :rtype: np.array
         """
 
         self.assign = np.zeros(self.X.shape[0], dtype=np.dtype('int32'))
         cluster = 1
-        for i, x in enumerate(self.X):
+        for i, _ in enumerate(self.X):
             if not self.assign[i]:
-                if self.expand_cluster(x, i, cluster):
+                if self.expand_cluster(i, cluster):
                     cluster += 1
         self.expand_clusters()
         return self.assign
 
-    def expand_cluster(self, x, x_indice, cluster):
+    def expand_cluster(self, x_indice: int, cluster: int) -> bool:
+        """
+        Permet de créer un nouveau groupe autour de l'objet `x` si |Rk(x)| >= k
+        Sinon retourne `False`
+
+        :param x_indice: l'indice de l'objet `x` dans `self.X`
+        :type x_indice: int
+        :param cluster: le numéro de cluster
+        :type cluster: int
+        :return: retourne faux si l'objet `x` n'a pas un score de voisinage suffisant pour créer un groupe
+        :rtype: bool
+        """
         
         if self.size_Rk[x_indice] < self.k:
             self.assign[x_indice] = -1
             return False
         else:
-            # seeds = queue.Queue()
-            # seeds.put(self.neighborhood(x, x_indice))
-            seeds = self.neighborhood(x, x_indice)
+            seeds = self.neighborhood(x_indice)
             self.assign[x_indice] = cluster
             self.assign[seeds] = cluster
-            for y in seeds:
-                y_indice = np.unique(np.where(self.X == y)[0])[0]
+            seeds_list = seeds.tolist()
+            while len(seeds_list) > 0:
+                y_indice = seeds_list.pop(0)
                 if self.size_Rk[y_indice] >= self.k:
-                    for z in self.neighborhood(y, self.k):
-                        z_assign = self.assign[np.unique(np.where(self.X == z)[0])]
+                    for z_indice in self.neighborhood(y_indice):
+                        z_assign = self.assign[z_indice]
                         # if z is unclassified
                         if z_assign == 0:
-                            seeds = np.concatenate((seeds, [z]))
-                            z_assign = cluster
+                            if z_indice not in seeds_list:
+                                seeds_list.append(z_indice)
+                            self.assign[z_indice] = cluster
                         # if z is noise
                         elif z_assign == -1:
-                            z_assign = cluster
+                            self.assign[z_indice] = cluster
             return True
 
-    def neighborhood(self, x, x_indice):
+    def neighborhood(self, x_indice: int) -> np.array:
         """
-        retourne l'ensemble des k voisins de `x` 
-        et également ses voisins inverses qui sont hubs
+        Retourne l'ensemble des k voisins de `x` 
+        et également ses voisins inverses qui sont hubs (cad |Rk| >= k)
+
+        :param x_indice: l'indice de l'objet `x`
+        :type x_indice: int
+        :return: les indices neighborhood
+        :rtype: np.array
         """
-        x_indice = np.where(self.X == x)[0]
         rNN_x = enum_rnn(self.indices, x_indice)
+        if rNN_x.size == 0:
+            return self.indices[x_indice]
         rNN_x = rNN_x[self.size_Rk[rNN_x] >= self.k]
-        return np.concatenate((self.indices[x_indice], rNN_x), axis=None)
+        return np.unique(np.concatenate((self.indices[x_indice][1:], rNN_x), axis=None))
 
     def expand_clusters(self):
-        for x in self.X:
-            x_indice = np.unique(np.where(self.X == x)[0])
-            if self.assign[x_indice][0] == -1:
-                neighbors = self.indices[x_indice][0]
+        """
+        Étend les groupes.
+        Pour les objets `x` classées comme anormaux : on regarde ses knn.
+        Sous certaines conditions (un voisin hub, ...), `x` peut finalement appartenir à un groupe.
+        
+        """
+        # TODO for x_indice, x in enumerate(self.X[self.assign == -1]):
+        for x_indice, x in enumerate(self.X):
+            if self.assign[x_indice] == -1:
+                neighbors = self.indices[x_indice]
                 mincluster = -1
                 mindist = float('inf')
-                for n in self.X[neighbors]:
-                    n_indice = np.where(self.X == n)[0]
-                    cluster = self.assign[n_indice[0]]
+                for n_indice in neighbors:
+                    n = self.X[n_indice]
+                    cluster_num = self.assign[n_indice]
+                    cluster = self.X[np.where(self.assign == cluster_num)]
                     d = euclidean(x, n)
-                    if self.size_Rk[n_indice[0]] >= self.k and d <= density_cluster(self.X[cluster]) and d < mindist:
-                        mincluster = cluster
+                    if self.size_Rk[n_indice] >= self.k and d <= density_cluster(cluster) and d < mindist:
+                        mincluster = cluster_num
                         mindist = d
                 self.assign[x_indice] = mincluster
     
 if __name__ == "__main__":
 
     # paramètre
-    k = 10
+    k = 20
     # dataset
     dataset = np.concatenate((np.random.normal(loc=0.0, scale=1.0, size=(9_000, 2)), 
                               np.random.normal(loc=6.0, scale=1.0, size=(1_000, 2))), axis=0)
-    my_clustering = RNN_DBSCAN(k, dataset)
-    my_clustering.rnn_dbscan()
+    my_clustering = RNN_DBSCAN(X=dataset, k=k)
+    
+    labels = my_clustering.rnn_dbscan()
+
+    # TODO bcp d'anomalie en faisant varier k ? groupe cohérent ? quelles sont les limites de la méthodes ?
+    # c'est long l'étape `expand_clusters` ? à évaluer
+    print(np.unique(labels, return_counts =True))
